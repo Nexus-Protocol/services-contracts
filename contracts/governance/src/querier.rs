9087850224df92c36f6518a23e0c7359205fb361
Stanislav Pirx <stanislav.pirx@gmail.com>
2021-08-30 13:22:22 +0300

    fix WasmQuery::Raw call to cw20 token, to get balance
    Signed-off-by: Stanislav Pirx <stanislav.pirx@gmail.com>

--- contracts/governance/src/querier.rs
+++ contracts/governance/src/querier.rs
@@ -1,18 +1,19 @@
 use cosmwasm_std::{Addr, Binary, Deps, QueryRequest, StdResult, Uint128, WasmQuery};
+use cosmwasm_storage::to_length_prefixed;
 
 pub fn query_token_balance(
     deps: Deps,
     contract_addr: &Addr,
     account_addr: &Addr,
 ) -> StdResult<Uint128> {
-    // load balance form the token contract
+    // load balance form the cw20 token contract version 0.6+
     Ok(deps
         .querier
         .query(&QueryRequest::Wasm(WasmQuery::Raw {
             contract_addr: contract_addr.to_string(),
             key: Binary::from(concat(
-                b"balance",
-                (deps.api.addr_canonicalize(account_addr.as_str())?).as_slice(),
+                &to_length_prefixed(b"balance"),
+                account_addr.as_bytes(),
             )),
         }))
         .unwrap_or_else(|_| Uint128::zero()))
